85d22e8cc9faf87bb9f6e6ad5ede2ad1082d82ee
MoazHassain <moazhassain@gmail.com>
2021-01-12 22:17:47 +0600

    web component practice

--- web-component/user-card.js
+++ web-component/user-card.js
@@ -1,8 +1,31 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+        h3{
+            color: coral;
+        }
+    
+    
+    </style>
+
+    <div class="user-card">
+        <h3></h3>
+    </div>
+
+
+
+
+`
+
+
 class userCard extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `<style>h3 {color: coral}</style>
-        <h3>${this.getAttribute("name")}</h3>`;
+
+        this.attachShadow({ mode:"open"});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector("h3").innerText=this.getAttribute("name");
+
     }
 }
 
